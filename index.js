ab67a3658535abd7cffe4a18484cfcc4a7d05162
gkrieg <gkrieg13@gmail.com>
2016-01-12 10:12:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // setup Express
-var app = require('./models/express.js');
+var express = require('express');
+var path = require('path');
 
+var portnum = 3000;
+var app = express();
 // start the server
-var server = app.listen(3000, function() {
+var server = app.listen(portnum, function() {
 console.log("Started on port 3000");
 var host = server.address().address;
 var port = server.address().port;
 });
+app.use(express.static(__dirname + '/public'));
