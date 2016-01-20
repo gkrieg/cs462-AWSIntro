fd422c62ee5cbf4cdbceadf861d09b5a2d89612f
gkrieg <gkrieg13@gmail.com>
2016-01-20 13:50:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ express.static.mime.define({"text/html": ["byu"]});
 app.post('/headers', function(req, res) {
   var headers = req.headers;
   var body = req.body;
-  var params = JSON.stringify(req.params);
+  var params = JSON.stringify(req.query);
   var together  = JSON.stringify(headers) + "\n" + JSON.stringify(body) + "\n" + params;
     res.send(together);
 });
