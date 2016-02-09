a0ea3f5a6d0290ec55254836417380bfe5a8b126
Spencer Krieger <gkrieg13@gmail.com>
2016-02-09 10:24:29 -0700

    oauth works

--- index.js
+++ index.js
@@ -1,55 +1,122 @@
-// setup Express
-var express = require('express');
-var path = require('path');
-var cookie-parser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , FoursquareStrategy = require('passport-foursquare').Strategy
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , session = require('express-session');
+
+var FOURSQUARE_CLIENT_ID = "4ZWLPRNEK3EKR4XFDIQHHPSOSTGSGRMYIE0OW3WQODZODCUT"
+var FOURSQUARE_CLIENT_SECRET = "W0CBJBGBTE3HWVGM0C51M1QVTWAWOVOCVUEW4YDXCKV0AKMY";
 
 
-var portnum = 3000;
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Foursquare profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FoursquareStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Foursquare
+//   profile), and invoke a callback with a user object.
+passport.use(new FoursquareStrategy({
+    clientID: FOURSQUARE_CLIENT_ID,
+    clientSecret: FOURSQUARE_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/foursquare/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Foursquare profile is returned
+      // to represent the logged-in user.  In a typical application, you would
+      // want to associate the Foursquare account with a user record in your
+      // database, and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
+
+
 var app = express();
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-// start the server
-var server = app.listen(portnum, function() {
-console.log("Started on port 3000");
-var host = server.address().address;
-var port = server.address().port;
-});
-app.use(express.static(__dirname + '/public'));
-express.static.mime.define({"text/html": ["byu"]});
+// configure Express
 
-app.post('/headers', function(req, res) {
-  var headers = req.headers;
-  var body = req.body;
-  var params = JSON.stringify(req.query);
-  var together  = JSON.stringify(headers) + "\n" + JSON.stringify(body) + "\n" + params;
-    res.send(together);
-});
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(cookieParser());
+  app.use(bodyParser());
+  app.use(session({ secret: 'keyboard cat' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.static(__dirname + '/public'));
 
-app.post('/redirect', function(req, res) {
-  var params = req.query;
-  console.log(params);
-  console.log(Object.keys(params));
-  const dict = {"foo": "http://www.google.com", "bar": "learningsuite.byu.edu"};
-  var redirectto = null;
-  for (var key in params) {
-    console.log(key);
-    if (key in dict) {
-    redirectto = dict[key];
-    res.redirect(redirectto);
-  }
-  }
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
 });
 
-  app.get('/version', function(req,res) {
-    var versionnum = req.headers["accept"];
-    console.log(req.headers);
-    console.log(versionnum);
-    if (versionnum == "application/vnd.byu.cs462.v1+json") {
-      res.send({"version": "v1" })
-    }
-    else if (versionnum == "application/vnd.byu.cs462.v2+json") {
-      res.send({"version": "v2" })
-    }
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/foursquare
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Foursquare authentication will involve
+//   redirecting the user to foursquare.com.  After authorization, Foursquare
+//   will redirect the user back to this application at /auth/foursquare/callback
+app.get('/auth/foursquare',
+  passport.authenticate('foursquare'),
+  function(req, res){
+    // The request will be redirected to Foursquare for authentication, so this
+    // function will not be called.
   });
+
+// GET /auth/foursquare/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/foursquare/callback',
+  passport.authenticate('foursquare', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(3000);
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
